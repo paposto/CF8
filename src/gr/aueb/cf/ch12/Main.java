e75818c40a3e8f3254e6411bb99eae870146012c
Petros Apostolopoulos <petrosapostolopoulos91@gmail.com>
2025-06-27 16:15:32 +0200

    feat: add student class and Main

--- src/gr/aueb/cf/ch12/Main.java
+++ src/gr/aueb/cf/ch12/Main.java
@@ -2,5 +2,16 @@ package gr.aueb.cf.ch12;
 
 public class Main {
 
-    public static
+    public static void main(String[] args) {
+
+        Student alice = new Student();
+
+        alice.id = 10;
+        alice.firstname = "Alice";
+        alice.lastname = "W";
+
+        System.out.println("ID: " + alice.id);                  //get id
+        System.out.println("Firstname " + alice.firstname);     //get firstname
+        System.out.println("Lastname " + alice.lastname);       //get lastname
+    }
 }
