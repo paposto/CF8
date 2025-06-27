103e06a200d08c547a8da766b57fb5c5468dcc8e
Petros Apostolopoulos <petrosapostolopoulos91@gmail.com>
2025-06-27 18:18:00 +0200

    fix: afm instead of vat

--- src/gr/aueb/cf/ch12/Customer.java
+++ src/gr/aueb/cf/ch12/Customer.java
@@ -4,7 +4,7 @@ public class Customer {
     private int id;
     private String firstname;
     private String lastname;
-    private String vat;
+    private String afm;
     private String phoneNo;
     private String region;
     private String prefecture;
@@ -17,11 +17,11 @@ public class Customer {
 
     }
 
-    public Customer(int id, String firstname, String lastname, String vat, String phoneNo, String region, String prefecture, String city, String street, String streetNo, String zipCode) {
+    public Customer(int id, String firstname, String lastname, String afm, String phoneNo, String region, String prefecture, String city, String street, String streetNo, String zipCode) {
         this.id = id;
         this.firstname = firstname;
         this.lastname = lastname;
-        this.vat = vat;
+        this.afm = afm;
         this.phoneNo = phoneNo;
         this.region = region;
         this.prefecture = prefecture;
@@ -31,4 +31,91 @@ public class Customer {
         this.zipCode = zipCode;
     }
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getAfm() {
+        return afm;
+    }
+
+    public void setAfm(String afm) {
+        this.afm = afm;
+    }
+
+    public String getPhoneNo() {
+        return phoneNo;
+    }
+
+    public void setPhoneNo(String phoneNo) {
+        this.phoneNo = phoneNo;
+    }
+
+    public String getRegion() {
+        return region;
+    }
+
+    public void setRegion(String region) {
+        this.region = region;
+    }
+
+    public String getPrefecture() {
+        return prefecture;
+    }
+
+    public void setPrefecture(String prefecture) {
+        this.prefecture = prefecture;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getStreetNo() {
+        return streetNo;
+    }
+
+    public void setStreetNo(String streetNo) {
+        this.streetNo = streetNo;
+    }
+
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
+    }
 }
