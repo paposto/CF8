92d75d5b7b86ecdec0d5010817c880c50c4d559a
Petros Apostolopoulos <petrosapostolopoulos91@gmail.com>
2025-06-30 15:02:59 +0200

    feat: add Account class (JavaBean)

---
+++ src/gr/aueb/cf/ch13/Account.java
@@ -0,0 +1,71 @@
+package gr.aueb.cf.ch13;
+
+public class Account {
+    private int id;
+    private String iban;
+    private String firstname;
+    private String lastname;
+    private String ssn;
+    private double balance;
+
+    public Account() {
+
+    }
+
+    public Account(int id, String iban, String firstname, String lastname, String ssn, double balance) {
+        this.id = id;
+        this.iban = iban;
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.ssn = ssn;
+        this.balance = balance;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getIban() {
+        return iban;
+    }
+
+    public void setIban(String iban) {
+        this.iban = iban;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getSsn() {
+        return ssn;
+    }
+
+    public void setSsn(String ssn) {
+        this.ssn = ssn;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+}
