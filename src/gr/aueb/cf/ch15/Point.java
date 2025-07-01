ff2a0f628149e2a5682f6315382524f44a194e26
Petros Apostolopoulos <petrosapostolopoulos91@gmail.com>
2025-07-01 14:54:10 +0200

    feat: add a point with one dimension

---
+++ src/gr/aueb/cf/ch15/Point.java
@@ -0,0 +1,21 @@
+package gr.aueb.cf.ch15;
+
+public class Point {
+    private double x;
+
+    public Point() {
+
+    }
+
+    public Point(double x) {
+        this.x = x;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+}
