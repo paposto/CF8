77708129355d3720bc0ea3464703e6e6937a5978
Petros Apostolopoulos <petrosapostolopoulos91@gmail.com>
2025-07-08 17:56:01 +0200

    feat: add account example with custom exceptions

---
+++ src/gr/aueb/cf/ch15/custom_exceptions/NegativeAmountException.java
@@ -0,0 +1,9 @@
+package gr.aueb.cf.ch15.custom_exceptions;
+
+public class NegativeAmountException extends Exception{
+
+    public NegativeAmountException(String message) {
+        super(message);
+    }
+
+}
---
+++ src/gr/aueb/cf/ch15/custom_exceptions/SsnNotValidException.java
@@ -0,0 +1,9 @@
+package gr.aueb.cf.ch15.custom_exceptions;
+
+public class SsnNotValidException extends Exception{
+
+    public SsnNotValidException(String message) {
+        super(message);
+
+    }
+}
