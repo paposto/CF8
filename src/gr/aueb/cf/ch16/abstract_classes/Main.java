fec334acc863e8e6e0a80e1ab0a7020afa49e856
Petros Apostolopoulos <petros.apostolopoulos@uni.lu>
2025-07-09 16:51:26 +0200

    feat: clear

---
+++ src/gr/aueb/cf/ch16/abstract_classes/Main.java
@@ -0,0 +1,9 @@
+package gr.aueb.cf.ch16.abstract_classes;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Animal cat = new Cat();
+        // Animal animal = new Animal((); error
+    }
+}
