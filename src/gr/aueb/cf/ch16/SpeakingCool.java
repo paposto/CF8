975b3aa21bd909ead939879972eb7d4f482601ad
Petros Apostolopoulos <petrosapostolopoulos91@gmail.com>
2025-07-09 13:33:39 +0200

    feat: add di (dependcy injection)

---
+++ src/gr/aueb/cf/ch16/SpeakingCool.java
@@ -0,0 +1,14 @@
+package gr.aueb.cf.ch16;
+
+public class SpeakingCool {
+
+    private final Cat cat = new Cat();
+
+    public SpeakingCool() {
+
+    }
+
+    public void learnToSpeak() {
+        cat.speak();
+    }
+}
