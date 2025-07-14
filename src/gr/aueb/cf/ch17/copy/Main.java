be5d62d221d3ac1b565c35a11808239067bc3e94
Petros Apostolopoulos <petros.apostolopoulos@uni.lu>
2025-07-14 12:13:26 +0200

    feat: add equals and hashcode

---
+++ src/gr/aueb/cf/ch17/copy/Main.java
@@ -0,0 +1,10 @@
+package gr.aueb.cf.ch17.copy;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Point p = new Point(1, 2);
+
+        Point copy = new Point(p);
+    }
+}
---
+++ src/gr/aueb/cf/ch17/copy/Point.java
@@ -0,0 +1,37 @@
+package gr.aueb.cf.ch17.copy;
+
+public class Point {
+    private int x;
+    private int y;
+
+    public Point() {
+
+    }
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    //Copy Constructor
+    public Point(Point point) {
+        this.x = point.getX();
+        this.y = point.getY();
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+}
