23604e62e90391c34d5ba535cdb92f33e253d2b7
Petros Apostolopoulos <petros.apostolopoulos@uni.lu>
2025-07-14 12:12:21 +0200

    feat: add single linked list

---
+++ src/gr/aueb/cf/ch18/singly_linked_list/Node.java
@@ -0,0 +1,33 @@
+package gr.aueb.cf.ch18.singly_linked_list;
+
+/**
+ * List node.
+ * @param <T> παραμετρικός τύπος.
+ */
+
+public class Node <T> {
+    private T item;
+    private Node<T> next;
+
+    public Node(Node<T> next, T item) {
+        this.next = next;
+        this.item = item;
+    }
+
+    public T getItem() {
+        return item;
+    }
+
+    public void setItem(T item) {
+        this.item = item;
+    }
+
+    public Node<T> getNext() {
+        return next;
+    }
+
+    public void setNext(Node<T> next) {
+        this.next = next;
+    }
+
+
